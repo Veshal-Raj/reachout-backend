3cc1d1ffea9880cb93cf5e3d75a8f372f0cf3d91
Veshal Raj <veshalraj1307@gmail.com>
2025-06-26 08:46:27 +0530

    update: adding profile pic, name from the google user info

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -13,3 +13,19 @@ export const validateEmail = (email) => {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
 };
+
+export const extractNameParts = (fullName) => {
+  const nameParts = fullName.split(' ');
+
+  let firstName = '';
+  let lastName = '';
+
+  if (nameParts.length === 1) {
+    firstName = nameParts[0];
+  } else if (nameParts.length > 1) {
+    firstName = nameParts[0];
+    lastName = nameParts.slice(1).join(' '); // handles middle names too
+  }
+
+  return { firstName, lastName };
+}
